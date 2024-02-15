824795400b3338a4f7b2d2ff801851ccda1aef53
Josh Polansky <josh@loupe.team>
2024-02-15 14:17:25 -0800

    Add configuration
    Improve messages for bad configuration

--- src/LuxServer/src/server/server.cpp
+++ src/LuxServer/src/server/server.cpp
@@ -23,24 +23,67 @@ int main(int argc, char const* argv[])
 
     jsonserver server;
 
-    if (cfg["serverType"].s() == "ADS") {
-#ifdef _WIN32
-        adsdatasrc* dataSource = new adsdatasrc();
-        dataSource->setLocalAms(cfg["adsParameters"]["localAmsNetId"].s());
-        dataSource->setPlcCommunicationParameters(cfg["adsParameters"]["ipV4Address"].s(), cfg["adsParameters"]["netID"].s(), cfg["adsParameters"]["port"].i());
-        dataSource->readPlcData();
+    // Add data sources to the server
+    //  Currently only supports ADS
+    try{
+        if (cfg["serverType"].s() == "ADS") {
+            std::string localAmsNetId = "127.0.0.1.1.1";
+            std::string ipV4Address;
+            std::string netID;
+            int port = 851;
+            try{
+                localAmsNetId = cfg["adsParameters"]["localAmsNetId"].s();
+            }catch(std::exception e){
+                cerr << "Could not read localAmsNetId from configuration file. Using default 127.0.0.1.1.1";
+            }   
+            try{
+                ipV4Address = cfg["adsParameters"]["ipV4Address"].s();
+                }catch(std::exception e){
+                cerr << "Could not read target ipV4Address from configuration file";
+                return 0;
+            }
+            try{
+                netID = cfg["adsParameters"]["netID"].s();
+            }catch(std::exception e){
+                cerr << "Could not read netID from configuration file";
+                return 0;
+            }
+            try{
+                port = cfg["adsParameters"]["port"].i();
+            }catch(std::exception e){
+                cerr << "Could not read port from configuration file, using default port 851";                
+            }
 
-        server.addDataSource(dataSource);
-#else
-        cerr << "ADS is not supported on this platform";
+            adsdatasrc* dataSource = new adsdatasrc();
+            dataSource->setLocalAms(localAmsNetId);
+            dataSource->setPlcCommunicationParameters(ipV4Address, netID, port);
+            dataSource->readPlcData();
+            server.addDataSource(dataSource);
+        } else {
+            cerr << "Unsupported server type";
+            return 0;
+        }
+    }catch(std::runtime_error e){
+        cerr << "Could not setup data source, check configuration file for errors.\n";
+        cerr << e.what();
+        cerr << "\n";
         return 0;
-#endif
-    } else {
-        cerr << "Unsupported server type";
+    }
+    catch(std::exception e){
+        cerr << "Could not setup data source, check configuration file for errors.\n";
+        cerr << e.what();
+        cerr << "\n";
         return 0;
     }
 
-    server.start(8000, false);
+    // Start the websocket server
+    int port = 8000;
+    try{
+        port = cfg["webSocket"]["port"].i();
+    }catch(std::exception e){
+        cerr << "Could not read port from configuration file, using default port 8000";
+    }
+    server.start(port, false);
 
     return 0;
 }
